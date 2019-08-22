37d17f571e425c09978cebf90059f9d5381aebb8
agent1894 <zhanzhaorui_94@163.com>
2019-08-23 01:31:37 +0800

    modified test.py, rm csv

--- Untitled.py
+++ Untitled.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 #YBB计算函数
 from math import fabs,exp,log
 import numpy as np
 import pandas as pd
+test=pd.read_csv('000001.csv')
 
 elapse = 0.0000001
 
-def ybb_equal(a, b): 
+def ybb_Equal(a, b): 
     return fabs(a-b) <= elapse or a==b
 
 
@@ -24,11 +23,11 @@ def ybb_Smaller(a, b):
 
 
 def ybb_GreaterOrEqual(a, b): 
-    return ybb_greater(a, b) or ybb_equal(a, b)
+    return ybb_Greater(a, b) or ybb_Equal(a, b)
 
 
 def ybb_SmallerOrEqual(a, b): 
-    return ybb_smaller(a, b) or ybb_equal(a, b)
+    return ybb_Smaller(a, b) or ybb_Equal(a, b)
 
 
 #3021_2_minus_c_devide_c_4h
@@ -119,3 +118,28 @@ for cbar in range(545):
     ybb_frama(close_, 21, pieces, capacity, cbar, ybb_outReal_base_2)
     ybb_minus_c_devide_c_extend(ybb_outReal_base_2, open_t, capacity, cbar, ybb_outReal_extend_2)
 
+ybb_outReal_extend_2
+
+len(ybb_outReal_extend_2)
+
+
+
+length=21
+test=pd.read_csv('000001.csv')
+test=test[['date_','open_t','close_']]
+test['current_value']=test['close_'].shift(length)
+test['current_temp']=test['close_'].shift(length-1)
+test['roll21_max']=test['current_temp'].rolling(length).max()
+test['roll21_min']=test['current_temp'].rolling(length).min()
+test['roll11_max']=test['current_temp'].shift(length//2).rolling(length//2+1).max()
+test['roll11_min']=test['current_temp'].shift(length//2).rolling(length//2+1).min()
+test['roll10_max']=test['current_temp'].rolling(length//2).max()
+test['roll10_min']=test['current_temp'].rolling(length//2).min()
+test['n3']=test.apply(lambda x:(max(x['current_temp'],x['roll21_max'])-min(x['current_temp'],x['roll21_min']))/length,axis=1)
+test['n2']=test.apply(lambda x:(max(x['current_temp'],x['roll11_max'])-min(x['current_temp'],x['roll11_min']))/length*2,axis=1)
+test['n1']=test.apply(lambda x:(max(x['current_temp'],x['roll10_max'])-min(x['current_temp'],x['roll10_min']))/length*2,axis=1)
+test['alpha']=test.apply(lambda x:np.exp(-4.6*((np.log(x['n1']+x['n2'])-np.log(x['n3']))/np.log(2)-1)) if x['n1']>0 and x['n2']>0 and x['n3']>0 else np.nan,axis=1)
+test['current_adj']=test['alpha']*test['close_']+test['current_value']*(1-test['alpha'])
+test['current_adj'].iloc[0:41]=test['close_'].iloc[0:41]
+test['indicator']=test.apply(lambda x:x['current_adj']/x['open_t']-1 if x['open_t']!=0 else 0,axis=1)
+test['indicator']
