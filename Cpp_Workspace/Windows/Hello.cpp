5952948b2bb65612ba6b11112128d056da9732fb
agent1894 <zhanzhaorui_94@163.com>
2019-09-06 23:59:59 +0800

    modify hello.cpp

--- Cpp_Workspace/Windows/Hello.cpp
+++ Cpp_Workspace/Windows/Hello.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World"<<endl;
+    cout << "Hello World" << endl;
+    cout << fixed; // 避免科学记数法
+    cout << setprecision(2); // 设置精度
+    double number;
+    number = 10.0/3*100000;
+    /*
+    * VS Code下Code Runner编译运行会出现中文乱码
+    * CMD运行可正常显示
+    */
+    cout << "保留两位小数数值为：" << number << endl; 
     return 0;
-} // namespace nam std;
+} 
