460c6f8e4310d74697877d83a34df22b034dab84
Zhaorui.Zhan <zhaorui.zhan@hemufunds.com>
2020-03-05 18:10:15 +0800

    start focusing on source package and modules

---
+++ Python_Workspace/QuantSim/source/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+"""
+For all types of data in any sources, the compulsory fields are:
+Code, DateTime, Open, High, Low, Close, Volume, Amount
+"""
