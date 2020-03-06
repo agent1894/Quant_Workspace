9468709a18cc21467a24187e2e3408f98ae6ab6b
Zhaorui.Zhan <zhaorui.zhan@hemufunds.com>
2020-03-06 18:08:44 +0800

    modify source package

--- Python_Workspace/QuantSim/source/__init__.py
+++ Python_Workspace/QuantSim/source/__init__.py
@@ -5,3 +5,4 @@
 For all types of data in any sources, the compulsory fields are:
 Code, DateTime, Open, High, Low, Close, Volume, Amount
 """
+__all__ = ["baostock"]
--- Python_Workspace/QuantSim/source/baostock.py
+++ Python_Workspace/QuantSim/source/baostock.py
@@ -8,6 +8,7 @@ The raw data should be feed to other module for further processing, only pd.Data
 from abc import ABCMeta, abstractmethod
 import pandas as pd
 import baostock as bs
+from enum import Enum
 
 
 # TODO: TWAP/VWAP price calculation. move to utility package
@@ -16,16 +17,31 @@ import baostock as bs
 #  thus there should be an option for switch on/off
 
 
+class DivAdjType(Enum):
+    Backward = '1'
+    backward = '1'
+    BACKWARD = '1'
+    Forward = '2'
+    forward = '2'
+    FORWARD = '2'
+    Non = '3'
+    non = '3'
+    NON = '3'
+
+
 class BaoStock(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, symbols: list, startDate: str, endDate: str, use_unique_fields: bool = False):
+    def __init__(self, symbols: list, startDate: str, endDate: str, dividend_adjustment: str, use_unique_fields: bool):
+        if not isinstance(symbols, list):
+            raise TypeError("symbols should be list type.")
         self.symbols = symbols
         self.startDate = startDate
         self.endDate = endDate
+        self.dividend_adjustment = dividend_adjustment
         self.use_unique_fields = use_unique_fields
         lg = bs.login()
-        if lg.error_msg != '0':
+        if lg.error_code != '0':
             print("login respond error_code: ", lg.error_code)
             print("login respond error_msg: ", lg.error_msg)
             raise RuntimeError("BaoStock login failed, try to subscribe another data sources.")
@@ -42,8 +58,10 @@ class BaoStock(object):
 
 
 class GetDailyBars(BaoStock):
-    def __init__(self, symbols: list, startDate: str, endDate: str, use_unique_fields: bool):
-        super(GetDailyBars, self).__init__(symbols, startDate, endDate, use_unique_fields)
+    def __init__(self, symbols: list, startDate: str, endDate: str,
+                 dividend_adjustment: str = DivAdjType.NON.value,
+                 use_unique_fields: bool = False):
+        super(GetDailyBars, self).__init__(symbols, startDate, endDate, dividend_adjustment, use_unique_fields)
 
     def get_k_bars(self):
         data = []
@@ -51,17 +69,20 @@ class GetDailyBars(BaoStock):
             singleData = []
             if not self.use_unique_fields:
                 rs = bs.query_history_k_data_plus(symbol, "date, code, open, high, low, close, volume, amount",
-                                                  start_date=self.startDate, end_date=self.endDate)
+                                                  start_date=self.startDate, end_date=self.endDate, frequency='D',
+                                                  adjustflag=self.dividend_adjustment)
             else:
                 rs = bs.query_history_k_data_plus(symbol,
                                                   "date, code, open, high, low, close, volume, amount, preclose, "
                                                   "adjustflag, turn, tradestatus, pctChg, peTTM, psTTM, pcfNcfTTM, "
                                                   "pbMRQ, isST",
-                                                  start_date=self.startDate, end_date=self.endDate)
+                                                  start_date=self.startDate, end_date=self.endDate, frequency='D',
+                                                  adjustflag=self.dividend_adjustment)
             while (rs.error_code == '0') & rs.next():
                 singleData.append(rs.get_row_data())
             dfResult = pd.DataFrame(singleData, columns=rs.fields)
-        return dfResult
+            data.append(dfResult)
+        return pd.concat(data)
 
     def feed_k_bars(self):
         pass
