4d51136f47f341c36105fb113687e97c9276fd23
Zhaorui.Zhan <zhanzhaorui_94@163.com>
2020-05-04 02:18:53 +0800

    update portfolio manager, solve the fatel problem of changing positions and percents

--- Python_Workspace/Sketches/PortfolioManager/PortfolioManager.py
+++ Python_Workspace/Sketches/PortfolioManager/PortfolioManager.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
+"""Portfolio manager
+This project works for a friend works in ZOFund.
+
+With given funds' net asset value and initial investment,
+this project will calculate the PnL and drawdowns for given portfolio positions.
+
+In future, this project should be compatible with Wind API.
+"""
 
 from math import isclose
 import warnings
@@ -12,7 +20,6 @@ import matplotlib.pyplot as plt
 import prettytable as pt
 
 warnings.filterwarnings("ignore")
-
 
 # TODO: for further updates, use Wind Python API for better performance
 # import WindPy as wp
@@ -32,10 +39,23 @@ class InstrType(Enum):
 
 
 class Instrument(object):
+    """Define instrument class for portfolio
+
+    When the portfolio has to change the positions of the instruments in it,
+    giving these instruments an abstract class may improved the performance.
+
+    Attributes:
+        name (Chinese): string, for users output and display.
+        code: string, which corresponds to the instrument, usually for double check.
+        instrType: InstrType, most of the time, the portfolio is constructed from some stocks and bonds funds.
+            this attribute can help check the corresponding type.
+        percnet: float, define the percentage of the instrument in the portfolio.
+    """
     def __init__(self, name: str, code: str, instrType=InstrType.Stock, percent: float = 0.0):
+        """The default constructor for Instrument gives the Stock type and 0% percent."""
         self.__name = name
         self.__code = code
-        self.__instrType = instrType
+        self.__type = instrType
         self._percent = percent
 
     @property
@@ -48,11 +68,11 @@ class Instrument(object):
 
     @property
     def instrType(self):
-        return self.__instrType
+        return self.__type
 
     @instrType.setter
     def instrType(self, value: str):
-        self.__instrType = InstrType[value]
+        self.__type = InstrType[value]
 
     @property
     def percent(self):
@@ -69,11 +89,13 @@ class Instrument(object):
 class Portfolio(object):
     def __init__(self, csv_path: str):
         self.__path = csv_path
-        self._stockPercent = float(input("Enter the percent of stocks: "))
-        assert (0.0 <= self._stockPercent <= 1.0)
-        self._bondPercent = 1.0 - self._stockPercent
+        self._stockPercent = 0.0
+        self._bondPercent = 0.0
         self._initialBalance = float(input("Enter the initial balance: "))
         assert (self._initialBalance >= 1)
+        self._alarmThreshold = 0.0
+        self._recoveryThreshold = 0.0
+        self._recoveryThresholdInterval = 0
         self.__encoding = "utf-8"
         try:
             with open(csv_path, encoding=self.__encoding) as f:
@@ -86,9 +108,10 @@ class Portfolio(object):
         instr = [item.split(',,,') for item in instr]
 
         orderedInstrs = OrderedDict(zip(instr[0], instr[1]))  # only for dataframe headers
-        print("Check the instrument codes: ")
+        print("*" * 10 + "Check the instrument codes: " + "*" * 10)
         for key in orderedInstrs.keys():
             print("Code for instrument {} is {}".format(key, orderedInstrs[key]))
+        print("*" * 48)
 
         self._df = pd.read_csv(csv_path, encoding=self.__encoding, header=3)
         self._df.dropna(inplace=True, axis=1)
@@ -107,24 +130,69 @@ class Portfolio(object):
             instrObj = Instrument(name=instr[0][i], code=instr[1][i])
             tempType = input("Enter the instrument {} type: ".format(instrObj.name))
             instrObj.instrType = tempType  # InstrType[tempType]
-            tempPercent = float(input("Enter the percent of the instrument {}: ".format(instrObj.name)))
-            instrObj.percent = tempPercent
             self._instrs.append(instrObj)
 
+    def set_percent(self):
+        self._stockPercent = float(input("Enter the percent of stocks: "))
+        assert (0.0 <= self._stockPercent <= 1.0)
+        self._bondPercent = 1.0 - self._stockPercent
+        for instr in self._instrs:
+            tempPercent = float(input("Enter the percent of the instrument {}: ".format(instr.name)))
+            instr.percent = tempPercent
         self._check_percents()
         self._display_instruments()
+
+    # TODO: add methods for defining thresholds
+    def set_parameters(self):
+        pass
 
     @property
     def stockPercent(self):
         return self._stockPercent
 
+    @stockPercent.setter
+    def stockPercent(self, value: float):
+        assert (0.0 <= value <= 1.0)
+        self._stockPercent = value
+        self._bondPercent = 1.0 - self._stockPercent
+
     @property
     def bondPercent(self):
         return self._bondPercent
 
+    @bondPercent.setter
+    def bondPercent(self, value: float):
+        assert (0.0 <= value <= 1.0)
+        self._bondPercent = value
+        self._stockPercent = 1.0 - self._bondPercent
+
     @property
     def initialBalance(self):
         return self._initialBalance
+
+    @property
+    def alarmThreshold(self):
+        return self._alarmThreshold
+
+    @alarmThreshold.setter
+    def alarmThreshold(self, value: float):
+        self._alarmThreshold = value
+
+    @property
+    def recoveryThreshold(self):
+        return self._recoveryThreshold
+
+    @recoveryThreshold.setter
+    def recoveryThreshold(self, value: float):
+        self._recoveryThreshold = value
+
+    @property
+    def recoveryThresholdInterval(self):
+        return self._recoveryThresholdInterval
+
+    @recoveryThresholdInterval.setter
+    def recoveryThresholdInterval(self, value: int):
+        self._recoveryThresholdInterval = value
 
     def _check_percents(self):
         stockPercent = 0.0
@@ -135,13 +203,11 @@ class Portfolio(object):
             else:
                 bondPercent += instr.percent
         if not isclose(stockPercent, self._stockPercent):
-            raise ValueError(
-                "The sum of stocks ({:.2%}) unequal to the given percent of stock ({:.2%}).".format(stockPercent,
-                                                                                                    self._stockPercent))
+            raise ValueError("The sum of stocks ({:.2%}) unequal to the given percent of stock ({:.2%}).".format(
+                stockPercent, self._stockPercent))
         if not isclose(bondPercent, self._bondPercent):
-            raise ValueError(
-                "The sum of bonds ({:.2%}) unequal to the given percent of bond ({:.2%}).".format(bondPercent,
-                                                                                                  self._bondPercent))
+            raise ValueError("The sum of bonds ({:.2%}) unequal to the given percent of bond ({:.2%}).".format(
+                bondPercent, self._bondPercent))
 
     def _display_instruments(self):
         tb = pt.PrettyTable()
@@ -150,27 +216,58 @@ class Portfolio(object):
             tb.add_row([instr.name, instr.code, instr.instrType, "{:.2%}".format(instr.percent)])
         print(tb)
 
-    def cal_plain_pnl(self):
-        for instr in self._instrs:
-            self._df.loc[:, "{}_ret".format(instr.name)] = self._df.loc[:, "{}_nav".format(instr.name)] / \
-                                                           self._df.iloc[0]["{}_nav".format(instr.name)] - 1.0
-            self._df.loc[:, "{}_value".format(instr.name)] = instr.percent * self._initialBalance * (
-                        self._df.loc[:, "{}_nav".format(instr.name)] / self._df.iloc[0]["{}_nav".format(instr.name)])
-        valueColumns = [item for item in self._df.columns if item.endswith('value')]
-        self._df.loc[:, "PnL"] = self._df.loc[:, valueColumns].sum(axis=1)
-        self._df.loc[:, "Ret"] = self._df.loc[:, "PnL"] / self._initialBalance - 1.0
-
-    def adjust_percent(self, instr):
-        pass
+    def _display_parameters(self):
+        tb = pt.PrettyTable()
+        tb.field_names = ["Parameter", "Value"]
+        tb.add_row(["Stock Percent", "{:.2%}".format(self._stockPercent)])
+        tb.add_row(["Bond Percent", "{:.2%}".format(self._bondPercent)])
+        tb.add_row(["Initial Balance", "${:.0f}".format(self._initialBalance)])
+        tb.add_row(["Alarm Threshold Percent", "{:.2%}".format(self._alarmThreshold)])
+        tb.add_row(["Recovery Threshold Percent", "{:.2%}".format(self._recoveryThreshold)])
+        tb.add_row(["Recovery Threshold Time Interval", "{:.0f} days".format(self._recoveryThresholdInterval)])
+        print(tb)
 
     @staticmethod
     def plot(series: pd.Series, begIndex: int = None, endIndex: int = None):
         plt.plot(series[begIndex:endIndex])
 
     @staticmethod
-    def cal_max_drawdown(series: pd.Series):
+    def cal_max_drawdown(series: pd.Series) -> (dt.datetime, dt.datetime, float):
         """Reference: https://blog.csdn.net/tz_zs/article/details/80335238"""
         endIndex = np.argmax(np.maximum.accumulate(series) - series)
         begIndex = np.argmax(series[:endIndex])
         maxDrawdown = series[begIndex] - series[endIndex]
-        return series, begIndex, endIndex, maxDrawdown
+        return begIndex, endIndex, maxDrawdown
+
+    def _alarm_drawdown_index(self, series: pd.Series) -> dt.datetime:
+        index = series[(np.maximum.accumulate(series) - series) >= self._alarmThreshold].index[0]
+        return index
+
+    def update_pnl(self, idx: dt.datetime = None) -> pd.DataFrame:
+        if idx is None:
+            balance = self._initialBalance
+            for instr in self._instrs:
+                self._df.loc[:, "{}_ret".format(instr.name)] = self._df.loc[:, "{}_nav".format(instr.name)] / \
+                                                            self._df.iloc[0]["{}_nav".format(instr.name)] - 1.0
+                self._df.loc[:, "{}_value".format(instr.name)] = balance * instr.percent * (
+                    self._df.loc[:, "{}_ret".format(instr.name)] + 1.0)
+                # (
+                #     df.loc[:, "{}_nav".format(instr.name)] / df.iloc[0]["{}_nav".format(instr.name)])
+        else:
+            prevIdx = self._df.index[np.argwhere(self._df.index == idx) - 1][0]
+            balance = self._df.loc[prevIdx, "PnL"].sum()
+            for instr in self._instrs:
+                self._df.loc[idx:, "{}_ret".format(instr.name)] = self._df.loc[
+                    idx:, "{}_nav".format(instr.name)] / self._df.loc[idx]["{}_nav".format(instr.name)] - 1.0
+                self._df.loc[idx:, "{}_value".format(instr.name)] = balance * instr.percent * (
+                    self._df.loc[:, "{}_ret".format(instr.name)] + 1.0)
+                # = balance * instr.percent * (
+                #     df.loc[:, "{}_nav".format(instr.name)] / df.iloc[0]["{}_nav".format(instr.name)])
+        self._df.loc[:, "PnL"] = self._df.loc[:,
+                                              [item for item in self._df.columns if item.endswith('value')]].sum(axis=1)
+        self._df.loc[:, "TotRet"] = self._df.loc[:, "PnL"] / self._initialBalance - 1.0
+        return self._df.PnL, self._df.TotRet
+
+
+if __name__ == "__main__":
+    pass
